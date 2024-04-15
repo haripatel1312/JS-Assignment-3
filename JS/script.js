12dfa2a2a79750cf17981272fdda1f06736b470e
haripatel1312 <137130311+haripatel1312@users.noreply.github.com>
2024-04-15 18:57:30 -0400

    My Changes

--- JS/script.js
+++ JS/script.js
@@ -5,14 +5,14 @@ fetch("https://jsonplaceholder.typicode.com/users")
     data.forEach(user => {
       const userDiv = document.createElement('div');
       userDiv.innerHTML = `
-        <h2>${user.name}</h2>
-        <p><strong>Username:</strong> ${user.username}</p>
-        <p><strong>Email:</strong> ${user.email}</p>
-        <p><strong>Address:</strong> ${user.address.street}, ${user.address.suite}, ${user.address.city}, ${user.address.zipcode}</p>
-        <p><strong>Phone:</strong> ${user.phone}</p>
-        <p><strong>Website:</strong> ${user.website}</p>
-        <p><strong>Company:</strong> ${user.company.name}</p>
-        <hr>
+      <h2>${user.name}</h2>
+      <p><i class="fas fa-user"></i> <strong>Username:</strong> ${user.username}</p>
+      <p><i class="fas fa-envelope"></i> <strong>Email:</strong> ${user.email}</p>
+      <p><i class="fas fa-map-marker-alt"></i> <strong>Address:</strong> ${user.address.street}, ${user.address.suite}, ${user.address.city}, ${user.address.zipcode}</p>
+      <p><i class="fas fa-phone"></i> <strong>Phone:</strong> ${user.phone}</p>
+      <p><i class="fas fa-globe"></i> <strong>Website:</strong> ${user.website}</p>
+      <p><i class="fas fa-building"></i> <strong>Company:</strong> ${user.company.name}</p>
+      <hr>
       `;
       userDataContainer.appendChild(userDiv);
     });
